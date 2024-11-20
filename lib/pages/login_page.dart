f61beee4e7e27a2193f6261c76da122c12f3b0ef
vunguyen-aya <vu.nguyen@ayahealthcare.com>
2024-11-20 15:44:32 -0600

    Fix login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -34,7 +34,7 @@ class _LoginPageState extends State<LoginPage> {
               children: [
                 // Logo or App Name
                 const Icon(
-                  Icons.lock,
+                  Icons.developer_mode,
                   size: 100,
                   color: Colors.blue,
                 ),
@@ -99,7 +99,7 @@ class _LoginPageState extends State<LoginPage> {
                   alignment: Alignment.centerRight,
                   child: TextButton(
                     onPressed: () {},
-                    child: const Text('Forgot Password?'),
+                    child: const Text('Forgot Password?', style: TextStyle(color: Colors.blue)),
                   ),
                 ),
                 const SizedBox(height: 24),
